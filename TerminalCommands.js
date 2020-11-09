d057a05cfbb8dd9f931ea458766e03067e9fbf59
ezikechris <christophermbe1009@gmail.com>
2020-11-09 10:00:22 -0500

    initial commit

---
+++ TerminalCommands.js
@@ -0,0 +1,22 @@
+/// Linux Terminal Command
+
+// ctrl + alt + T  open linux terminal //
+
+/// ctrl + D  Close Linux terminal //  
+
+/// cal 2017 for year date // 
+
+// cal -y also for calender for 2017//
+
+// date AND timezone etc just type date//
+
+//clear workspace by typing clear// 
+
+// type history for all commands used on startup//
+
+//running a number using !1 -!2 -!3 etc//
+
+// existing the terminal using typing command EXIT OR CTRL D//
+
+
+
---
+++ Terminals,Commands,&Shells.js
@@ -0,0 +1,4 @@
+// Commands Terminals & Shells ///
+//  Linux commands are interpreted via the linux Shell just like the brain interpretes letters or word///
+
+// Terminal is nothing more than a window to the shell//
